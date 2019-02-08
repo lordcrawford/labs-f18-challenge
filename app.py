95f6c6c69585281eaf8d1c6e7892136e54c13e8f
Lord Crawford <lrc2161@columbia.edu>
2019-02-07 22:56:23 -0500

    adi labs challenge

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -6,6 +8,17 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def main():
     return render_template('index.html')
+
+@app.route("/pokemon/<query>")
+def search(query):
+
+    url = "https://pokeapi.co/api/v2/pokemon-form/" + query + "/"
+    r = requests.get(url).json()
+
+    if query.isdigit():
+        return 'The pokemon with id ' + str(query) + ' is ' + r['name']
+    else:
+        return str(query) + ' has id ' +  str(r['id'])
 
 
 if __name__ == '__main__':
