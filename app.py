aedc61716446061050b2babf34bebfcaea189c5e
Lord Crawford <lrc2161@columbia.edu>
2019-02-07 22:56:36 -0500

    second

--- app.py
+++ app.py
@@ -13,13 +13,18 @@ def main():
 def search(query):
 
     url = "https://pokeapi.co/api/v2/pokemon-form/" + query + "/"
-    r = requests.get(url).json()
+    r = requests.get(url)
 
-    if query.isdigit():
-        return 'The pokemon with id ' + str(query) + ' is ' + r['name']
+    if r.status_code == 200:
+
+        r = r.json()
+
+        if query.isdigit():
+            return 'The pokemon with id ' + str(query) + ' is ' + r['name']
+        else:
+            return str(query) + ' has id ' +  str(r['id'])
     else:
-        return str(query) + ' has id ' +  str(r['id'])
-
+        return 'Please input a valid pokemon name or id'
 
 if __name__ == '__main__':
     app.run()
